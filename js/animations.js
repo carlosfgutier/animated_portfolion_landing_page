34bb6dd9b194988cb82b25cb4ad78451c1d2c7c1
CFG <carlosfgutier@gmail.com>
2020-05-04 12:27:12 -0500

    first commit

---
+++ js/animations.js
@@ -0,0 +1,3 @@
+setTimeout(function(){ 
+    $(".slide-1__h1").html("Hola, my name is Carlos.<div class='slide-1__sub'>Let's <span>build, </span><span> create, </span><span>blow minds, </span><span>grow.</span></div>"); 
+}, 12000);
